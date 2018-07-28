c4dbd7a1baafa1626e5c368bc5611474e4b6c1b2
razzester <razzester86@gmail.com>
2018-07-28 19:37:37 +0545

    Fetch PR Candidate

--- routes/candidate.js
+++ routes/candidate.js
@@ -13,6 +13,7 @@ var candidate=require('../controllers/candidate')
     router.get('/add', candidate.add_some_booths);
     // Get list of all fptp candidates based on type. Example Url: localhost:3500/candidate/fptp/hor    (Booth Id is must in the request header.)
     router.get('/fptp/:type', candidate.fetch_fptp_candidate);
+    router.get('/pr/:type', candidate.fetch_pr_candidate);
     router.get('/pr/all', candidate.get_all_pr);
 
 
