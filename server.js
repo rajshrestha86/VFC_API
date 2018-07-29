d4b5d5dcdb1b3647d6fbc0da9ba6751b5cb21632
sabin15 <sabinshrestha15@outlook.com>
2018-07-29 20:13:58 +0545

    pr_candidate model updated , fetch_pr_candidate updates and booth_address is added as a CORS header

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const options = {
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,booth_address');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
