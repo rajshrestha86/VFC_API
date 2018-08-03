18242539bada5900aea9d13c57cad20b6bbec2bf
rxyz58 <rajiv.shah01234@gmail.com>
2018-08-03 09:33:05 +0545

    counting done

--- models/districts.js
+++ models/districts.js
@@ -4,7 +4,8 @@ Schema=mongoose.Schema;
 var dist_schem=new Schema({
     district_name: {type: String},
     no_of_Hor: {type: Number},
-    no_of_prov:{type: Number}
+    no_of_prov:{type: Number},
+    state:{type:Number}
 
 });
 
